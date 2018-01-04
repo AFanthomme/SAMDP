729f3caf55c813a1fdb24e519db196d14e7d74a5
AntoineGoblet <35069011+AntoineGoblet@users.noreply.github.com>
2018-01-04 19:24:37 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,3 +1,15 @@
 '''
 This script designed to run some simple experiments using the tools from the src module
 '''
+'''
+This script designed to run some simple experiments using the tools from the src module
+'''
+import src.option_iteration
+from src.gridworld import GridWorld
+
+if __name__ == '__main__':
+
+    example = src.option_iteration.test_gridmaker()
+
+    example.aggregate_states(K=5,w=3)
+    example.show_clustering()
